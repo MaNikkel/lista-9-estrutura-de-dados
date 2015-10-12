2e4e027d91241c3b17d56afdda100b4c43cf27da
Mathias <mathiasnikkel@gmail.com>
2015-10-12 14:49:53 -0300

    exercicio 7 finalizado

--- ex7.c
+++ ex7.c
@@ -13,20 +13,43 @@ void menu(int *opcao)
 {
 	printf("1 - empilhar\n");
 	printf("2 - exibir em ordem inversa\n");
+	printf("0 - sair\n");
 	scanf("%d", opcao);
 		
+}
+void exibir()
+{
+	int i;
+	for(i = 0; i < topo2; i++)
+		printf("pilha[%d] = %d\n", i, pilha2[i]);
+}
+void preencherP2()
+{
+	topo1--;
+	do
+	{
+		pilha2[topo2] = pilha1[topo1];
+		topo1--;
+		topo2++;
+	}while(topo1 >= 0);
+	exibir();
 }
 int main()
 {
 	int num;
 	int op;
-	menu(&op);
-	
-	switch(op)
+	do
 	{
-		case 1:
-			printf("informe um numero a ser empilhado: ");
-			scanf("%d", &num);
-			push(num);
-	}
+		menu(&op);	
+		switch(op)
+		{
+			case 1:
+				printf("informe um numero a ser empilhado: ");
+				scanf("%d", &num);
+				push(num);
+				break;
+			case 2:
+				preencherP2();
+		}
+	}while(op != 0);
 }
