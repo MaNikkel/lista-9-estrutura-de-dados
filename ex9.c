774a386c7288b384ac7930b63d28e4c7bbf5c087
Mathias <mathiasnikkel@gmail.com>
2015-10-19 16:08:27 -0200

    ex9 concluído

--- ex9.c
+++ ex9.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 32
+int pilha[N], topo = 0;
 
+
+void converter(int num)
+{
+	while(num > 0)
+	{
+		pilha[topo] = num % 2;
+		num /= 2;
+		topo++;
+	}
+}
+
+int main()
+{
+	int num;
+	printf("escreva o numero a ser convertido: ");
+	scanf("%d", &num);
+	converter(num);
+	printf("numero em binario = ");
+	for(num = topo; num >= 0; num--)
+	{
+		printf("%d", pilha[num]);
+	}
+}
